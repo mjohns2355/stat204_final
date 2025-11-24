7040ef5dedf1b6c49661f0b728eb7a166b632d39
mjohns2355 <mljohns@ucsc.edu>
2025-11-24 15:35:44 -0800

    experimental tukey test

--- practice.R
+++ practice.R
@@ -12,3 +12,26 @@ survey_complete <- survey_response[complete.cases(survey_response),]
 ggplot(survey_complete, aes(x = skill, y = score)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE)
+
+score_skill <- aggregate(score ~ skill, data = survey_complete, FUN = mean)
+aggregate(score ~ like, data = survey_complete, FUN = mean)
+aggregate(score ~ intention, data = survey_complete, FUN = mean)
+aggregate(score ~ ai, data = survey_complete, FUN = mean)
+
+
+plot(score_skill)
+
+model <- aov(score ~ ai, survey_complete)
+summary(model)
+
+survey_complete$ai <- factor(survey_complete$ai)
+survey_complete$skill <- factor(survey_complete$skill)
+
+model2 <- aov(score ~ skill + ai + skill:ai, survey_complete)
+summary(model2)
+
+model3 <- aov(score ~ skill, survey_complete)
+summary(model3)
+
+tukey_results <- TukeyHSD(model2)
+print(tukey_results)
