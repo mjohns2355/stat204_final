70093d70976d2147957d20fbcadfda5bb4152240
Adam Khan <adamjyk23@gmail.com>
2025-11-25 13:25:00 -0800

    added zip data

--- ML_portion.R
+++ ML_portion.R
@@ -1,3 +1,6 @@
 # empty for now
 # extract data from other hanabi github first
 #maybe this will work
+library(tidyverse)
+ag_data <- read_csv("allgames.zip", "allgames.csv")
+features_data <- read_csv("features.zip", "features.csv")
