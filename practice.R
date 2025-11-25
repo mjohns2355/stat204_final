13961e0f599922b6f6ca088858a5a123d6085ef5
mjohns2355 <mljohns@ucsc.edu>
2025-11-24 16:11:10 -0800

    added some comments

--- practice.R
+++ practice.R
@@ -1,25 +1,36 @@
-# Load the coffee ratings data
+# Load the survey ratings data
 survey_response <- readr::read_csv('https://raw.githubusercontent.com/yawgmoth/HanabiData/refs/heads/master/participants.csv')
 
+# add some libraries
 library(ggplot2)
 library(dplyr)
 
 # Preview the data
 head(survey_response)
 
+# cleanup and remove NAs
 survey_complete <- survey_response[complete.cases(survey_response),]
 
+# plot skill vs score
 ggplot(survey_complete, aes(x = skill, y = score)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE)
 
+# aggregate score by perceived skill of AI
 score_skill <- aggregate(score ~ skill, data = survey_complete, FUN = mean)
+
+# aggregated score by whether they liked the AI
 aggregate(score ~ like, data = survey_complete, FUN = mean)
+
+# aggregate score by how intentional they thought the AI was
 aggregate(score ~ intention, data = survey_complete, FUN = mean)
+
+# aggregate score by which AI was used
 aggregate(score ~ ai, data = survey_complete, FUN = mean)
 
-
+# plot aggregated score by perceived skill
 plot(score_skill)
+
 
 model <- aov(score ~ ai, survey_complete)
 summary(model)
